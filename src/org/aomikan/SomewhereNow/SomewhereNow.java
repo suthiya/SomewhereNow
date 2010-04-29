acad5ccfe3b9f8cf30565996e93584d52912deae
mikan <webmaster@aomikan.org>
2010-04-30 01:59:45 +0900

    project initalize

---
+++ src/org/aomikan/SomewhereNow/SomewhereNow.java
@@ -0,0 +1,13 @@
+package org.aomikan.SomewhereNow;
+
+public class SomewhereNow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello world!");
+	}
+
+}
